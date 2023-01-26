45342aa9ddaaa1473d69f2608313efebdeb2184d
BootCampTech <mjunaid@live.co.uk>
2023-01-26 13:20:39 +0000

    Descriptions added to functions in the script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,13 @@
+var saveBtn = $(".saveBtn");
+
 // function code
 function updateTime() {
   let today = moment();
 
-  // updates the time element in the header
+// updates the time element in the header
   $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm.ss"));
 
-  // For coloring the past, present, and future time blocks
+// For coloring the past, present, and future time blocks
   let now = moment().format("kk");
   for (let i = 0; i < scheduleElArray.length; i++) {
       scheduleElArray[i].removeClass("future past present");
@@ -20,8 +22,65 @@ function updateTime() {
 
           scheduleElArray[i].addClass("future");
       }
+
+    }
   }
+
+  function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+// console.log(this); //each time-block
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+
+    })
+  };  
+
+// WHEN I click the save button for that time block
+saveBtn.on("click", function() {
+
+// console.log(this); //save button
+  var time = $(this).siblings(".hour").text();
+  var plan = $(this).siblings(".plan").val();
+
+// THEN the text for that event is saved in local storage
+  localStorage.setItem(time, plan);
+});
+
+// WHEN I refresh the page
+// THEN the saved events persist
+function usePlanner() {
+
+  $(".hour").each(function() {
+      var currHour = $(this).text();
+      var currPlan = localStorage.getItem(currHour);
+
+// console.log(this);
+// console.log(currHour);
+
+      if(currPlan !== null) {
+          $(this).siblings(".plan").val(currPlan);
+      }
+  });
 }
+
+/**
+* CALL FUNCTIONS
+*/
+
+timeBlockColor();
+usePlanner();
+
+
 
 // textarea elements
 let saveButton = $(".save-icon");
@@ -57,13 +116,15 @@ function renderLastRegistered() {
   for (let el of scheduleElArray) {
       el.val(localStorage.getItem("time block " + el.data("hour")));
 
+      
   }
 }
 
 
 // function for handling clicks
 function handleFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
+
 
   let btnClicked = $(event.currentTarget);
 
@@ -72,9 +133,14 @@ function handleFormSubmit(event) {
   let targetTimeBlock = targetText.data("hour");
 
   localStorage.setItem("time block " +  targetTimeBlock, targetText.val());
+
+  
+ 
 }
 
 saveButton.on("click", handleFormSubmit);
+
+
 
 
 
